e28f0e5314dc891521cf910f91f5fc6452be0dd0
MarlonGhostk <36806611+MarlonGhostk@users.noreply.github.com>
2023-10-12 17:53:45 -0500

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/'],
+  protectedRoutes: ['/Academia X', '/Aura Prods', '/Comunidad Reparando', '/Crehana', '/Domestika', '/Domestika', '/Domestika', '/Mastermind', '/Mentorial' , '/Mentorial', '/Platzi', '/Roco PS', '/The Fullstack Devs', '/Udemy', '/.password'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: "",
