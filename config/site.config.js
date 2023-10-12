632042ae4a01e7eff371a3e09e73adf964bb7ec7
MarlonGhostk <36806611+MarlonGhostk@users.noreply.github.com>
2023-10-12 17:14:02 -0500

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -17,7 +17,7 @@ module.exports = {
   kvPrefix: process.env.KV_PREFIX || '',
 
   // The name of your website. Present alongside your icon.
-  title: "MGhostk OneDrive",
+  title: "GTKDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/Cursos',
